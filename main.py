0a8179641d0711d988551ae01d59a1ae7a152af1
dhruvburman97 <63149531+dhruvburman97@users.noreply.github.com>
2020-11-15 15:15:39 +0530

    diff impl altogether

--- main.py
+++ main.py
@@ -1,207 +1,66 @@
-# Password Manager
+import sqlite3
+from hashlib import sha256
 
-### LIBRARIES ###
+MASTERPW = "dhruv97"
 
-try:
-	import hashlib as h
-	import datetime as d
-	import secrets as s
-except ImportError:
-	print("Error... necessary libraries are unavailable.")
+PASSWORD = input("ENTER THE MASTER PASSWORD :-")
 
-### END LIBRARIES ###
+while MASTERPW != PASSWORD:
+    if MASTERPW != PASSWORD:
+        print("Invalid Password\n")
+        break
+    
+if MASTERPW == PASSWORD:
+        print("WELCOME BACK SIR :)")
 
-### FUNCTIONS ###
+conn = sqlite3.connect('pass_manager.db')
 
+def create_password(pass_key, service, admin_pass):
+    return sha256(admin_pass.encode('utf-8') + service.lower().encode('utf-8') + pass_key.encode('utf-8')).hexdigest()[:15]
 
-def randomInt(bits):  # Returns a strong random integar of "bits" bits
-	try:
-		num = s.randbits(bits)
-		return num
-	except:
-		print("Error... please try again.")
+def get_hex_key(admin_pass, service):
+    return sha256(admin_pass.encode('utf-8') + service.lower().encode('utf-8')).hexdigest()
 
+def get_password(admin_pass, service):
+    secret_key = get_hex_key(admin_pass, service)
+    cursor = conn.execute("SELECT * from KEYS WHERE PASS_KEY=" + '"' + secret_key + '"')
 
-def randomStr(x):  # Returns a strong random string of length "x" of characters selected from "dictionary"
-	try:
-		dictionary = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-_+=@[](),./#~!£$%^&*:;"
-		string = ""
-		for i in range(x):
-			string += s.choice(dictionary)
-		return string
-	except:
-		print("Error... please try again.")
+    file_string = ""
+    for row in cursor:
+        file_string = row[0]
+    return create_password(file_string, service, admin_pass)
 
+def add_password(service, admin_pass):
+    secret_key = get_hex_key(admin_pass, service)
 
-def gen_sha3512_hash(x):  # Returns the SHA3-512 hash of "inp"
-	try:
-		obj = h.sha3_512()
-		obj.update(x.encode())
-		return str(obj.hexdigest())
-	except:
-		print("Error... please try again.")
+    command = 'INSERT INTO KEYS (PASS_KEY) VALUES (%s);' %('"' + secret_key +'"')        
+    conn.execute(command)
+    conn.commit()
+    return create_password(secret_key, service, admin_pass)
 
+if MASTERPW == PASSWORD:
+    try:
+        conn.execute('''CREATE TABLE KEYS
+            (PASS_KEY TEXT PRIMARY KEY NOT NULL);''')
+        print("Your safe has been created!\nWhat would you like to store in it today?")
+    except:
+        print("You have a safe, what would you like to do today?")
+    
+    
+    while True:
+        print("\n"+ "*"*15)
+        print("Commands:")
+        print("Press 1 : TO Genrate a Password")
+        print("Press 2 : To Get Stored Password")
+        print("Press 3 : Quit")
+        print("*"*15)
+        input_ = input(":")
 
-def genSalt():  # Returns a stronly random 512 bit salt
-	try:
-		randomness = str(randomInt(4096)) + randomStr(4096)
-		salt = gen_sha3512_hash(randomness)
-		return salt
-	except:
-		print("Error... please try again.")
-
-
-def addUser(username, password):  # Hashes, salts, and stores credentials in their respective files#
-	try:
-		usernames = []
-
-		with open("user.db", "rt") as userFile:  # Fills the array "usernames"
-			for userLine in userFile:
-				usernames.append(userLine.strip("\n"))
-			userFile.close()
-
-		p = open("pass.db", "a")  # Open the database files
-		s = open("salt.db", "a")
-		u = open("user.db", "a")
-
-		salt = genSalt()  # Generate hashes to store
-		userHash = gen_sha3512_hash(username)
-		passSalt = password + salt
-		passHash = gen_sha3512_hash(passSalt)
-
-		available = True
-
-		for i in range(len(usernames)):
-			if usernames[i] == userHash:  # Check if the username is available
-				available = False
-
-				salt = ""  # Clear variables
-				userHash = ""
-				passSalt = ""
-				passHash = ""
-
-		if available == True:
-			p.write(passHash + "\n")  # Write values to files
-			s.write(salt + "\n")
-			u.write(userHash + "\n")
-
-			salt = ""  # Clear variables
-			userHash = ""
-			passSalt = ""
-			passHash = ""
-
-			p.close()  # Close files
-			s.close()
-			u.close()
-
-			print("Your credentials have been added!\n")
-
-		elif available == False:
-			print("That username is taken! Please try again...\n")
-
-		else:
-			print("Error! Please try again...\n")
-
-		salt = ""  # Clear variables
-		userHash = ""
-		passSalt = ""
-		passHash = ""
-
-		p.close()  # Close files
-		s.close()
-		u.close()
-	except:
-		print("Error... please try again.")
-
-
-def login(username, password):  # Verifies the user entered "username" and "password", and prints "Logged in!" if they're in the database
-	# Test Credentials: "testUser", "testPass"
-
-	try:
-		usernames = []  # Define credential lists
-		passwords = []
-		salts = []
-
-		with open("pass.db", "rt") as passFile:  # Fill lists with credentials
-			for passLine in passFile:
-				passwords.append(passLine.strip("\n"))
-			passFile.close()
-
-		with open("salt.db", "rt") as saltFile:
-			for saltLine in saltFile:
-				salts.append(saltLine.strip("\n"))
-			saltFile.close()
-
-		with open("user.db", "rt") as userFile:
-			for userLine in userFile:
-				usernames.append(userLine.strip("\n"))
-			userFile.close()
-
-		# Generate "username" hash for comparison & lookup
-		userHash = gen_sha3512_hash(username)
-		salt = ""
-		passHash = ""
-		location = 0  # Location of credentials in list
-		found = False
-
-		go = True
-		while go:
-			for i in range(len(usernames)):  # Look up location of credentials
-				if usernames[i] == userHash:
-					location = i
-					found = True
-					go = False
-			go = False
-
-		if found == True:  # Look up the rest of the credentials
-			salt = salts[location]
-			passSalt = password + salt
-			passHash = gen_sha3512_hash(passSalt)
-
-		# Verify the credentials and return True if correct
-		if userHash == usernames[location] and passHash == passwords[location]:
-			print("Logged in!\n")
-			#TO-DO: Make a accounts.db, table should have parent_username, account_title, username, password
-			#TO-DO: Check if username of this user is not in accounts.db and add it if not, else get their accounts or ask them if they want to add a new account. 
-			#TO-DO: Query the DB for all their accounts here
-			#TO-DO: Print them, ask for input, show password as per
-		elif userHash != usernames[location] or passHash != passwords:
-			print("Credentials not found! Please try again...\n")
-
-		usernames = []  # Clear credential lists
-		passwords = []
-		salts = []
-
-		userHash = ""
-		salt = ""
-		passHash = ""
-		location = 0
-	except:
-		print("Error... please try again.")
-
-### END FUNCTIONS ###
-
-### MAIN PROGRAM ###
-	
-
-while True:
-	try:
-		option = int(input("Enter 1 to sign up\nEnter 2 to login\nEnter 3 to exit\n: "))
-
-		if option == 1:
-			username = input("\nPlease choose a username: ")
-			password = input("Please enter a strong password for your account: ")
-			addUser(username, password)
-	
-		elif option == 2:
-			username = input("\nPlease enter your username: ")
-			password = input("Please enter your password: ")	
-			login(username, password)
-			
-		elif option == 3:
-			break
-	except:
-		print("Error... please try again.\n")
-		break
-
-### END MAIN PROGRAM ###
+        if input_ == "3":
+            break
+        if input_ == "1":
+            service = input("What is the name of the service?\n")
+            print("\n" + service.capitalize() + " password created:\n" + add_password(service, MASTERPW))
+        if input_ == "2":
+            service = input("What is the name of the service?\n")
+            print("\n" + service.capitalize() + " password:\n" +get_password(MASTERPW, service))
